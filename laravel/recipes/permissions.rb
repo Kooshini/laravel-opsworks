f5d5f708244604dc98430ba9c019ab79d34aa6da
Kooshini <kooshini@gmail.com>
2015-04-01 22:59:38 +0100

    ensure correct users were being set on files
    deploy:www-data

--- laravel/recipes/permissions.rb
+++ laravel/recipes/permissions.rb
@@ -2,6 +2,7 @@
 node[:deploy].each do |app_name, deploy|
 	if platform_family?('debian')
 		execute "set permissions for #{deploy[:deploy_to]}/current/app/storage/" do
+		command "sudo chown deploy:www-data -R #{deploy[:deploy_to]}/current/app/storage/"
 		command "sudo chmod 0777 -Rf #{deploy[:deploy_to]}/current/app/storage/"
 	  end
 	end
